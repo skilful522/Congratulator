362c6b0f805c5ae7c538039c8c700207831c5272
Stanislav Sergienya <skilful522@gmail.com>
2022-10-05 18:01:58 +0300

    finalized project

---
+++ src/getCardTemplate.js
@@ -0,0 +1,30 @@
+export const getCardTemplate = () => {
+  return `<div class="card">
+      <div class="outside">
+        <div class="front">
+          <p>С днём рождения!</p>
+          <div class="cake">
+            <div class="top-layer"></div>
+            <div class="middle-layer"></div>
+            <div class="bottom-layer"></div>
+            <div class="candle"></div>
+          </div>
+        </div>
+        <div class="back">
+          <div class="image-wrapper">
+            <img
+              class="person-avatar"
+              src=""
+            />
+          </div>
+        </div>
+      </div>
+      <div class="inside">
+        <p>
+          Поздравляю тебя с днём рождения, <span class="person-name"></span>, желаю тебе крепкого здоровья,
+          удачи, ярких дней, исполнения всех твоих желаний и огромной любви!!!
+        </p>
+        <h1>&#127873;</h1>
+      </div>
+    </div>`;
+};
---
+++ src/index.js
@@ -0,0 +1,75 @@
+import happyBirthday from "../assets/happyBirthday.mp3";
+import "./styles.css";
+
+import { getCardTemplate } from "./getCardTemplate";
+
+const getElementsByClass = (selector) =>
+  document.querySelectorAll("." + selector);
+
+const body = document.querySelector("body");
+
+const user_id = 168326438;
+
+let isPlayMusic = false;
+
+VK.init({
+  apiId: 51433402,
+});
+
+VK.Api.call(
+  "friends.get",
+  {
+    user_id,
+    v: "5.131",
+    fields: ["bdate", "name_case", "photo_200_orig"],
+  },
+  ({ response: { items } }) => {
+    const date = new Date();
+    const currentDay = date.getDate();
+    const currentMonth = date.getMonth();
+    const currentDate = `${currentDay}.${currentMonth}`;
+
+    const personsHasBirthdayToday = items.filter(({ bdate }) => {
+      if (bdate) {
+        const [bday, bmonth] = bdate.split(".");
+
+        return currentDate === `${bday}.${bmonth}`;
+      }
+    });
+
+    if (personsHasBirthdayToday.length > 0) {
+      isPlayMusic = true;
+    }
+
+    if (personsHasBirthdayToday.length === 0) {
+      body.innerHTML = '<p class="empty-list">Сегодня нет именниников =(</p>';
+    }
+
+    personsHasBirthdayToday.forEach(() => {
+      body.innerHTML = body.innerHTML + getCardTemplate();
+    });
+
+    const personAvatars = getElementsByClass("person-avatar");
+    const personNames = getElementsByClass("person-name");
+
+    personsHasBirthdayToday.forEach(({ first_name, photo_200_orig }) => {
+      personNames.forEach((personName) => {
+        personName.innerHTML = first_name;
+      });
+      personAvatars.forEach((personAvatar) => {
+        personAvatar.src = photo_200_orig;
+      });
+    });
+  }
+);
+
+addEventListener(
+  "click",
+  () => {
+    if (isPlayMusic) {
+      const audio = new Audio(happyBirthday);
+      audio.play();
+    }
+  },
+  { once: true }
+);

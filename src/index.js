3a2b3de8841b1f5a68e2b9fea6f209e1a7da6b1f
Stanislav Sergienya <skilful522@gmail.com>
2022-10-06 00:46:45 +0300

    fix for max

--- src/index.js
+++ src/index.js
@@ -18,56 +18,88 @@ VK.init({
 
 const MAX_ID = 114030939;
 
-VK.Api.call(
-  "friends.get",
-  {
-    user_id,
-    v: "5.131",
-    fields: ["bdate", "name_case", "photo_200_orig"],
-  },
-  ({ response: { items } }) => {
-    const date = new Date();
-    const currentDay = date.getDate();
-    const currentMonth = date.getMonth() + 1;
-    const currentDate = `${currentDay}.${currentMonth}`;
+const max = {
+  id: 114030939,
+  photo_200_orig:
+    "https://sun2.beltelecom-by-minsk.userapi.com/s/v1/if1/bQW6t5tpTfnJAyB0W7hSs_vt8cxRxrzdal72xqk1yYNAyqcBkkKkkz5hDLzF3MB-cV1HPGlC.jpg?size=200x355&quality=96&crop=0,0,1215,2160&ava=1",
+  track_code:
+    "b80a8cd1HbyRlcUzCanvmwYKIwrPu7DA8JN1l0rEsd1p-FQShXBw157xrzINrLnJJpCw43zSw9Tpn2L5Lrfu",
+  first_name: "Max",
+  last_name: "Pavlusevich",
+  can_access_closed: true,
+  is_closed: false,
+};
 
-    const personsHasBirthdayToday = items.filter(({ id, bdate }) => {
-      if (id === MAX_ID) {
-        return currentDate.toString() === '6.10';
-      }
+// VK.Api.call(
+//   "friends.get",
+//   {
+//     user_id,
+//     v: "5.131",
+//     fields: ["bdate", "name_case", "photo_200_orig"],
+//   },
+//   ({ response: { items } }) => {
+//     const date = new Date();
+//     const currentDay = date.getDate();
+//     const currentMonth = date.getMonth() + 1;
+//     const currentDate = `${currentDay}.${currentMonth}`;
+//
+//     const personsHasBirthdayToday = [max];
+//
+//     // const personsHasBirthdayToday = items.filter(({ id, bdate }) => {
+//     //   if (id === MAX_ID) {
+//     //     return currentDate.toString() === '6.10';
+//     //   }
+//     //
+//     //   if (bdate) {
+//     //     const [bday, bmonth] = bdate.split(".");
+//     //
+//     //     return currentDate === `${bday}.${bmonth}`;
+//     //   }
+//     // });
+//
+//     if (personsHasBirthdayToday.length > 0) {
+//       isPlayMusic = true;
+//     }
+//
+//     if (personsHasBirthdayToday.length === 0) {
+//       body.innerHTML = '<p class="empty-list">Сегодня нет именинников =(</p>';
+//     }
+//
+//     personsHasBirthdayToday.forEach(() => {
+//       body.innerHTML = body.innerHTML + getCardTemplate();
+//     });
+//
+//     const personAvatars = getElementsByClass("person-avatar");
+//     const personNames = getElementsByClass("person-name");
+//
+//     personsHasBirthdayToday.forEach(({ first_name, photo_200_orig }) => {
+//       personNames.forEach((personName) => {
+//         personName.innerHTML = first_name;
+//       });
+//       personAvatars.forEach((personAvatar) => {
+//         personAvatar.src = photo_200_orig;
+//       });
+//     });
+//   }
+// );
 
-      if (bdate) {
-        const [bday, bmonth] = bdate.split(".");
-
-        return currentDate === `${bday}.${bmonth}`;
-      }
-    });
-
-    if (personsHasBirthdayToday.length > 0) {
-      isPlayMusic = true;
-    }
-
-    if (personsHasBirthdayToday.length === 0) {
-      body.innerHTML = '<p class="empty-list">Сегодня нет именниников =(</p>';
-    }
+const personsHasBirthdayToday = [max];
 
     personsHasBirthdayToday.forEach(() => {
       body.innerHTML = body.innerHTML + getCardTemplate();
     });
 
-    const personAvatars = getElementsByClass("person-avatar");
-    const personNames = getElementsByClass("person-name");
+const personAvatars = getElementsByClass("person-avatar");
+const personNames = getElementsByClass("person-name");
 
-    personsHasBirthdayToday.forEach(({ first_name, photo_200_orig }) => {
-      personNames.forEach((personName) => {
-        personName.innerHTML = first_name;
-      });
-      personAvatars.forEach((personAvatar) => {
-        personAvatar.src = photo_200_orig;
-      });
-    });
-  }
-);
+personsHasBirthdayToday.forEach(({ first_name, photo_200_orig }) => {
+  personNames.forEach((personName) => {
+    personName.innerHTML = first_name;
+  });
+  personAvatars.forEach((personAvatar) => {
+    personAvatar.src = photo_200_orig;
+  });
+});
 
 addEventListener(
   "click",
